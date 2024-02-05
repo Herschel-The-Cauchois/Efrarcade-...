5f06a7c8d7041877b702fd2518d2c6b198230973
Agathe <idfc.gfy@gmail.com>
2024-02-05 15:29:12 +0100

    class Player

--- SpaceInvader.py
+++ SpaceInvader.py
@@ -1,24 +1,101 @@
 from pygame import *
+from pygame.locals import *
 
 """
 SPACE INVADER
 """
 
-init()  # Initializes pygame
+#INFOS
+"""
+  0-|------------ (x)
+    |
+    |
+    |
+   (y)
+"""
 
-# Creates a window with the name of the game, and sets the future background image
-display.set_caption("Efrarcade")
-scene = display.set_mode((1000, 500))
-background = image.load("./assets/test.png")
-
+#CONSTANTES
+L = 1000
+H = 500
 is_active = True
 
-# Main loop that will reproduce a series of checks while the window is deemed active
+#INITIALISATION
+
+init()  
+
+#WINDOW
+display.set_caption("Efrarcade")
+scene = display.set_mode((L, H))
+background = image.load("./assets/test.png")
+
+#PLAYER
+perso = image.load("./assets/perso.png").convert()
+persoRect = perso.get_rect()
+persoRect.x = (L - 150)/2
+persoRect.y = H - 100
+
+
+
+#PLAYER MOVEMENT
+
+class Player:
+
+    def __innit__(self, x, y, ship):
+        self.x = x                              # x and y are the coordinates of the player's ship
+        self.y = y                     
+        self.ship = ship                        # The ship is the image of the player's spaceship
+        
+
+    def move(self):
+
+        l = L - 150
+        h = H - 100
+        #event = [K_RIGHT, K_LEFT, K_UP, K_DOWN]
+
+        
+        for event in event.get():          
+
+
+            if event.type == KEYDOWN:       # If a key is pressed, the player's ship moves
+
+                if event.key == K_RIGHT:    #Right arrow key
+                    if persoRect.x < l:
+                        persoRect.x += 1
+                    
+
+                if event.key == K_LEFT:     #Left arrow key
+                    if persoRect.x > 0:
+                        persoRect.x -= 1
+
+
+                if event.key == K_UP:       #Up arrow key
+                    if persoRect.y > 0:
+                        persoRect.y -= 1
+
+
+                if event.key == K_DOWN:     #Down arrow key
+                    if persoRect.y < h:
+                        persoRect.y += 1
+
+                
+
+
+
+
+
+
+#GAME LOOP
+
 while is_active:
     scene.blit(background, (0, 0))
-    display.flip()  # Sets the background and refreshes the window
+    scene.blit(perso,(persoRect.x, persoRect.y))
+    display.flip()                                  # Sets the background and refreshes the window
+    
+    Player.move(perso)
+
     for thing in event.get():
-        if thing.type == QUIT:
-            # If quitting event detected, closes the windows
+        if thing.type == QUIT:                      # If the window is closed, the game stops
             is_active = False
             quit()
+
+
