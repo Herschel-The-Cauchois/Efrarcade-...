8fbdab488d2a84ffb850a695c20200279659355f
Agathe <idfc.gfy@gmail.com>
2024-02-05 15:58:23 +0100

    moving while the key is pressed

--- SpaceInvader.py
+++ SpaceInvader.py
@@ -19,6 +19,7 @@ SPACE INVADER
 global L, H, is_active
 global scene, background, perso, persoRect
 
+clock = pygame.time.Clock()
 L = 1000
 H = 500
 is_active = True
@@ -54,30 +55,32 @@ class Player:
 
         l = L - 150                            # The player's ship can't go beyond the window
         h = H - 100
+        
 
         
         for event in pygame.event.get():          
 
             if event.type == pygame.KEYDOWN:       # If a key is pressed, the player's ship moves
+                
 
-                if event.key == K_RIGHT:    #Right arrow key
+                if event.key == K_RIGHT:           #Right arrow key
                     if persoRect.x < l:
-                        persoRect.x += 5
+                        persoRect.x += 10
                     
 
-                if event.key == K_LEFT:     #Left arrow key
+                if event.key == K_LEFT:            #Left arrow key
                     if persoRect.x > 0:
-                        persoRect.x -= 5
+                        persoRect.x -= 10
 
 
-                if event.key == K_UP:       #Up arrow key
+                if event.key == K_UP:              #Up arrow key
                     if persoRect.y > 0:
-                        persoRect.y -= 5
+                        persoRect.y -= 20
 
 
-                if event.key == K_DOWN:     #Down arrow key
+                if event.key == K_DOWN:            #Down arrow key
                     if persoRect.y < h:
-                        persoRect.y += 5
+                        persoRect.y += 20
 
                 
 
@@ -92,7 +95,9 @@ while is_active:
     scene.blit(background, (0, 0))
     scene.blit(perso,(persoRect.x, persoRect.y))
     display.flip()                                  # Sets the background and refreshes the window
-    
+    clock.tick(30)
+
+
     Player.move(perso)
 
     for thing in event.get():
