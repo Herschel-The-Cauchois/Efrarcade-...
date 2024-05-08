40f99099267e45dcb544a4520ee83cec603f79f4
Pverte <63103558+Pverte@users.noreply.github.com>
2024-05-08 21:50:00 +0200

    Merge pull request #10 from Herschel-The-Cauchois/info_bar
    Added Info Bar with the Level, HP, and Highscore (presentation, does not work for now)

---
+++ test_tests.py
@@ -0,0 +1,22 @@
+import pygame
+import sys
+scene=pygame.display.set_mode((1000, 1000))
+def level_bar(player_level, player_xp):
+    """Creates a rectangle that will show in a % the player's level progression."""
+    # Fill the rectangle with the player's level progression
+    pygame.draw.rect(scene, (0, 255, 0), (0, 0, (player_xp/100)*100, 25))
+    # Draw the border rectangle
+    pygame.draw.rect(scene, (255, 255, 255), (0, 0, 100, 25), 2)
+
+
+def game_loop():
+    """Main game loop."""
+    while True:
+        level_bar(1, 50)
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+game_loop()
