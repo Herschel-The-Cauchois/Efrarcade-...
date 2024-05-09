ec8eacb04c6a78efd2bcaf011e06fac328d90fbc
Herschel_TC <raphael.lesterlin@gmail.com>
2024-05-09 22:56:58 +0200

    Added pre launch arrow rotation fully implemented

--- BeerPong.py
+++ BeerPong.py
@@ -8,12 +8,13 @@ def bp_game_loop(username: str):
     background = transform.scale(background, (700, 500))
     is_active = True  # Elementary boolean that stays True until QUIT event is triggered.
     game = Game()
-    game.vector.graphical_rotation(0, 1, game.ball.rect.center)
+    game.vector.graphical_rotation(0, 1, game.ball)  # Does the initial rotation of the arrow-vector representing
+    # the trajectory's input.
     while is_active:
         scene.blit(background, (0, 0))  # Draws background.
         game.game_sprites.update()
         game.game_sprites.draw(scene)
-        display.flip()
+        display.flip()  # Draws every graphical element of the game.
 
         for thing in event.get():
             if thing.type == QUIT:
@@ -21,16 +22,18 @@ def bp_game_loop(username: str):
                 is_active = False
                 quit()
             if thing.type == KEYDOWN:
+                # Before the launch of the ball, lets the player adjust the acceleration that the ball will have
+                # as well as its angle to maximise the accuracy of the trajectory.
                 if thing.key == K_LEFT:
-                    game.vector.graphical_rotation(game.vector.angle + 1, game.vector.acceleration, game.ball.rect.center)
+                    game.vector.graphical_rotation(game.vector.angle + 1, game.vector.acceleration, game.ball)
                 if thing.key == K_RIGHT:
                     game.vector.graphical_rotation(game.vector.angle - 1, game.vector.acceleration,
-                                                   game.ball.rect.center)
+                                                   game.ball)
                 if thing.key == K_UP:
                     game.vector.graphical_rotation(game.vector.angle, game.vector.acceleration + 1,
-                                                   game.ball.rect.center)
+                                                   game.ball)
                 if thing.key == K_DOWN:
                     game.vector.graphical_rotation(game.vector.angle, game.vector.acceleration - 1,
-                                                   game.ball.rect.center)
+                                                   game.ball)
 
 bp_game_loop("Test")
--- BeerPong_classes.py
+++ BeerPong_classes.py
@@ -5,65 +5,63 @@ from SpaceInvader_enemies import bezier_curve_calc  # For displaying curve traje
 
 class PlayerGlass(sprite.Sprite):
     def __init__(self):
+        """Initializes the glass as a simple graphic sprite."""
         super().__init__()
         self.image = image.load("./assets/Water glass.png")
         self.image = transform.scale(self.image, (100, 100))
         self.rect = self.image.get_rect()  # Creates hit box
-        # self.rect = Rect.inflate(self.rect, -15, -15) To see if we need rect redimensioning
         self.rect.x = 0
         self.rect.y = 400
 
 
 class GoalGlass(sprite.Sprite):
     def __init__(self):
+        """Initializes the goal glasses' sprite."""
         super().__init__()
         self.image = image.load("./assets/Water glass.png")
         self.image = transform.scale(self.image, (100, 100))
-        self.rect = self.image.get_rect()  # Creates hit box
-        # self.rect = Rect.inflate(self.rect, -15, -15) To see if we need rect redimensioning
+        self.rect = self.image.get_rect()
         self.rect.x = 600
         self.rect.y = 400
 
 
 class Ball(sprite.Sprite):
     def __init__(self, player_glass_coord: tuple):
+        """Initializes the Ball's sprite."""
         super().__init__()
         self.image = image.load("./assets/The BallTM.png")
-        self.image = transform.scale(self.image, (25, 25))
+        self.image = transform.scale(self.image, (25, 25))  # Rescales the sprite.
         self.rect = self.image.get_rect()  # Creates hit box
         # self.rect = Rect.inflate(self.rect, -15, -15) To see if we need rect redimensioning
-        self.rect.center = player_glass_coord
+        self.rect.center = player_glass_coord  # Centers it around the top of the player's glass.
 
 
 class Vector(sprite.Sprite):
-    def __init__(self):
+    def __init__(self, ball_topright: tuple):
         super().__init__()
         self.image = image.load("./assets/vecteur test.png")
+        self.orig_image = self.image  # Keeps a copy of the original image to avoid loading it at each transformation.
         self.image = transform.scale(self.image, (25, 25))
-        self.rect = self.image.get_rect()  # Creates hit box
-        # self.rect = Rect.inflate(self.rect, -15, -15) To see if we need rect redimensioning
-        self.rect.x = 50  # Puts the vector at a default place
-        self.rect.y = 390
-        self.angle = 90
+        self.rect = self.image.get_rect(center=ball_topright)  # Puts the trajectory arrow near and above the ball for schematisation.
+        self.pos = Vector2(ball_topright)  # Data for rotation management.
+        self.offset = Vector2(10, -10)
+        self.angle = 0  # Holds the essential data that will be used for the trajectory calculation.
         self.acceleration = 1
 
-    def graphical_rotation(self, angle: int, length: int, center: tuple):
+    def graphical_rotation(self, angle: int, length: int, ball):
         if 0 <= angle < 91 and 1 <= length <= 10:
-            self.image = image.load("./assets/vecteur test.png")
             self.angle = angle
-            self.acceleration = length
-            self.image = transform.rotate(self.image, angle)
+            self.acceleration = length  # Modifies the input data with the player's modifications.
+            # Applies length and angle transformation to the arrow. To preserve its position, the offset vectors once
+            # Rotated allows a recalibration of the position due to the rect modifications induced by image rotation.
+            self.image = transform.rotozoom(self.orig_image, self.angle, 1)
             self.image = transform.scale(self.image, (25+length*5, 25+length*5))
-            self.rect = self.image.get_rect()
-            if 0 <= angle <= 4:
-                self.rect.midleft = center
-            elif 4 < angle <= 65:
-                print(int(sin(radians(angle))*75))
-                self.rect.bottomleft = (center[0], center[1]+20)
-            elif 75 <= angle <= 90:
-                self.rect.midbottom = center
+            offset_rotation = self.offset.rotate(angle)
+            self.rect = self.image.get_rect(center=self.pos+offset_rotation)  # Places the arrow at the desired
+            # Corrected position.
             return True
         else:
+            # If the player's modification induces entering wrong values, doesn't do anything.
             return False
 
 
@@ -74,7 +72,7 @@ class Game:
         self.player_glass = PlayerGlass()  # Instantiates the glass that will represent the player's.
         self.glass_goal = GoalGlass()  # Instantiates the glass surrounded by a few rects
         self.ball = Ball(self.player_glass.rect.midtop)
-        self.vector = Vector()
+        self.vector = Vector(self.ball.rect.topright)
         self.game_sprites.add(self.player_glass)  # Adds successfully each sprite to the sprite group for display.
         self.game_sprites.add(self.glass_goal)
         self.game_sprites.add(self.ball)
